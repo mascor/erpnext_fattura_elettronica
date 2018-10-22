42e4cde96d14bd27e1fc471b946e8b608a926891
Gaurav <gvrvnk@gmail.com>
2018-10-22 17:22:23 +0530

    [WIP] Unit Test

--- erpnext_fattura_elettronica/erpnext_fattura_elettronica/doctype/efe_xml_export/test_efe_xml_export.py
+++ erpnext_fattura_elettronica/erpnext_fattura_elettronica/doctype/efe_xml_export/test_efe_xml_export.py
@@ -5,6 +5,88 @@ from __future__ import unicode_literals
 
 import frappe
 import unittest
+from frappe import _
 
+
+def create_data():
+	
+	#Add a company address.
+	company_name = frappe.defaults.get_defaults()["company"]
+	
+	frappe.get_doc({
+		"doctype": "Address",
+		"title": "Indoril Nerevar-Billing",
+		"address_line1": "via Roma",
+		"city": "Foggia",
+		"state": "Puglia",
+		"fax": "123123456",
+		"links": [
+			{
+				"link_doctype": "Company",
+				"link_name": company_name
+			}
+		] 
+	}).insert()
+
+	#Create a customer with an address and contact.
+	frappe.get_doc({
+		"doctype": "Customer",
+		"customer_name": "Indoril Nerevar",
+		"customer_group": _("Individual"),
+		"territory": _("All Territories"),
+		"efe_codice_fiscale": "ABC123XCV123W",
+	}).insert()
+
+	frappe.get_doc({
+		"doctype": "Address",
+		"title": "Indoril Nerevar-Billing",
+		"address_line1": "via Roma",
+		"city": "Foggia",
+		"state": "Puglia",
+		"fax": "123123456",
+		"links": [
+			{
+				"link_doctype": "Customer",
+				"link_name":"Indoril Nerevar"
+			}
+		] 
+	}).insert()
+	
+	frappe.get_doc({
+		"doctype": "Contact",
+		"first_name": "Indoril",
+		"last_name": "Nerevar",
+		"email_id": "nerevar@vvardenfell.mw",
+		"phone": "24445555",
+		"state": "Puglia",
+		"links": [
+			{
+				"link_doctype": "Customer",
+				"link_name":"Indoril Nerevar"
+			}
+		] 
+	}).insert()
+
+	#Create three Items with different IVA Rates
+	#Create invoices
+	# 	1. First Customer, I1, I2
+	# 	2. Second Customer, I1, I3
+	# 	3. Second Customer, I1, I2, I3
+	
+	#Create Payment Entry for First Invoice
+
+def remove_data():
+	frappe.delete_doc("Customer", "Mario Rossi")
+	frappe.db.commit()
+	
 class TestEFEXMLExport(unittest.TestCase):
-	pass
+	def setUp(self):
+		create_data()
+
+	def tearDown(self):
+		#remove_data()
+		pass
+
+	def test_customer_created(self):
+		customer = frappe.get_doc("Customer", "Mario Rossi")
+		self.assertTrue(customer is not None)

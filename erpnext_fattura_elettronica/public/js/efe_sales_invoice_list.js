4ae2fa3089621eff10bc35b7907cc72569534c94
Gaurav <gvrvnk@gmail.com>
2018-12-14 16:38:44 +0530

    refactor(Custom Script): Used doctype_js instead of custom_scripts
    Renamed Sales Invoice.js to efe_sales_invoice.js, moved to public/js
    Removed console.log in efe_sales_invoice_list.js

--- erpnext_fattura_elettronica/public/js/efe_sales_invoice_list.js
+++ erpnext_fattura_elettronica/public/js/efe_sales_invoice_list.js
@@ -1,6 +1,5 @@
 frappe.listview_settings['Sales Invoice'] = {
     onload: function(listview) {
-      console.log(listview);
       listview.page.add_menu_item(__("Export XML"), () => {
           frappe.new_doc("EFE XML Export")
       });

9eecc396983e318faf0fc24a73e51a97125e0f4d
Gaurav <gvrvnk@gmail.com>
2019-01-14 11:05:17 +0530

    refactor(sales invoice): List form
    Updated code to work with overrides from multiple apps

--- erpnext_fattura_elettronica/public/js/efe_sales_invoice_list.js
+++ erpnext_fattura_elettronica/public/js/efe_sales_invoice_list.js
@@ -1,20 +1,18 @@
-frappe.listview_settings['Sales Invoice'] = {
-    refresh: function(listview) {
-        listview.page.add_menu_item(__("Export XML"), () => {
-            if (!listview.get_checked_items().length) {
-                frappe.msgprint(__("Please select at least one invoice to export."))
-                return
-            }
+frappe.listview_settings['Sales Invoice']["refresh"] = function(listview) {
+    listview.page.add_menu_item(__("Export XML"), () => {
+        if (!listview.get_checked_items().length) {
+            frappe.msgprint(__("Please select at least one invoice to export."))
+            return
+        }
 
-            var names = listview.get_checked_items().map(function(item) { return item.name });
-            var w = window.open(
-                frappe.urllib.get_full_url("/api/method/erpnext_fattura_elettronica.export_invoices.generate_electronic_invoices?"
-                + "names=" + encodeURIComponent(names))
-            );
-            if (!w) {
-                frappe.msgprint(__("Please enable pop-ups")); return;
-            }
-        });
-        
-    }
-};
+        var names = listview.get_checked_items().map(function(item) { return item.name });
+        var w = window.open(
+            frappe.urllib.get_full_url("/api/method/erpnext_fattura_elettronica.export_invoices.generate_electronic_invoices?"
+            + "names=" + encodeURIComponent(names))
+        );
+        if (!w) {
+            frappe.msgprint(__("Please enable pop-ups")); return;
+        }
+    });
+    
+}

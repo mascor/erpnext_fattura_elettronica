07ab7b4c8e05bed0d963810e2384e2f460c0845b
Gaurav <gvrvnk@gmail.com>
2019-01-15 15:26:51 +0530

    new(doctype): efe settings

---
+++ erpnext_fattura_elettronica/erpnext_fattura_elettronica/doctype/efe_settings/efe_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Massimiliano Corvino and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('EFE Settings', {
+	refresh: function(frm) {
+
+	}
+});

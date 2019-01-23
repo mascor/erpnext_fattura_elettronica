4059f5fb4067387a778ee435343914ea12436ba9
Gaurav <gvrvnk@gmail.com>
2019-01-24 00:28:22 +0530

    new(efe sales invoice): Set mode of payment in payment schedule from template/mode_of_payment

--- erpnext_fattura_elettronica/efe_sales_invoice.py
+++ erpnext_fattura_elettronica/efe_sales_invoice.py
@@ -1,12 +1,15 @@
 import frappe
 
 def validate(doc, method):
-    if not doc.payment_terms_template:
-        return
-    
-    template = frappe.get_doc("Payment Terms Template", doc.payment_terms_template)
-    for term in template.terms:
-        for row in doc.payment_schedule:
-            if term.payment_term == row.payment_term:
-                row.efe_mode_of_payment = term.efe_mode_of_payment
-                row.efe_bank_account = term.efe_bank_account
+    if not hasattr(doc, 'mode_of_payment'):
+        if doc.payment_terms_template:
+            template = frappe.get_doc("Payment Terms Template", doc.payment_terms_template)
+            for term in template.terms:
+                for row in doc.payment_schedule:
+                    if term.payment_term == row.payment_term:
+                        row.efe_mode_of_payment = term.efe_mode_of_payment
+                        row.efe_bank_account = term.efe_bank_account
+    else:
+        if doc.mode_of_payment:
+            for row in doc.payment_schedule:
+                row.efe_mode_of_payment = doc.mode_of_payment
